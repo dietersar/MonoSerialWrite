99739d9542726cf641af30f2a71d2f966c6caae2
Dieter Sarrazyn <dieter@secudea.be>
2021-06-21 09:44:55 +0200

    initial push

---
+++ MonoSerialWrite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonoSerialWrite
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
